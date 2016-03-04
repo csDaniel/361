cc8ad6cfab7da171ab1275f78f20ac6c17f554ab
Daniel OFarrell <ofarreld@onid.oregonstate.edu>
2016-03-04 13:06:20 +0900

    Maybe 50% done with seperating add.php into 2 seperate files. Just currently, the lookup from start to home doesnt do anything.

---
+++ addTest.php
@@ -0,0 +1,88 @@
+<?php
+  ini_set('display_errors', 'On');
+  error_reporting(E_ALL | E_STRICT);
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Create a new issue</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" media="screen" title="no title" charset="utf-8">
+  <link rel="stylesheet" href="css/master.css" media="screen" title="no title" charset="utf-8">
+</head>
+
+<body>
+  <div class='container'>
+    <div class='col-xs-12'>
+      <div class='page-header'>
+        <h1 class='text-center'>Create a new issue</h1>
+        <p>
+        </p>
+      </div>
+    </div>
+    <div class='col-xs-12 col-md-6 col-md-offset-3'>
+      <form id='newIssue'>
+        <div class='form-group'>
+          <label for='title'>Issue title</label>
+          <input type='text' placeholder='e.g. Idea for creating new jobs' name='title' class='form-control' required/>
+        </div>
+        <div class='form-group'>
+          <label for='category'>Issue category</label>
+          <select class='form-control' name='category' required>
+            <option selected disabled>Select a category</option>
+            <option>Civil Rights</option>
+            <option>Finance</option>
+            <option>Defense & Foreign Policy</option>
+            <option>Economy</option>
+            <option>Education</option>
+            <option>Energy</option>
+            <option>Environment</option>
+            <option>Elections</option>
+            <option>Health</option>
+            <option>Immigration</option>
+            <option>Student Loans</option>
+            <option>Transportation</option>
+            <option>Unions & Labor</option>
+          </select>
+        </div>
+        <div class='form-group'>
+          <label for='category'>Issue description</label>
+          <textarea class='form-control' name='description' placeholder='i.e. Describe the issue you are concerned about, including as much relevant detail as possible.' rows=10 required></textarea>
+        </div>
+        <!--<div class='form-group'>
+          <label for='geocomplete'>Street address (find your reps)</label>
+          <input id='geocomplete' class='form-control' name='location' />
+          <div class='geodata'>
+            <input name="lat" id='lat' type="hidden" value="">
+            <input name="lng" id='lng' type="hidden" value="">
+          </div>
+        </div>
+        <div class='form-group' id='official-group'>
+          <label for='officials'>Address to an official/representative</label>
+          <table class='table table-bordered' id="officialsTable">
+            <tbody>
+            </tbody>
+          </table>
+        </div> -->
+        <div class='form-group'>
+          <button type='submit' id='submitIssue' class='btn btn-success btn-lg btn-block'>
+            <span id='submitState'>Submit Issue</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js" charset="utf-8"></script>
+  <script src="http://maps.googleapis.com/maps/api/js?sensor=false&amp;libraries=places"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+  <script src="js/jquery.geocomplete.min.js"></script>
+  <script src="js/app.js"></script>
+</body>
+
+</html>
+
+<script>
+console.log(sessionStorage.getItem('testCode'));
+</script>
---
+++ homeTest.php
@@ -0,0 +1,113 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+<meta charset="utf-8">
+<title>Politics is Cool</title>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="http://maps.googleapis.com/maps/api/js?sensor=false&amp;libraries=places"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+  <script src="js/jquery.geocomplete.min.js"></script>
+  
+</head>
+<body>
+  <div class='container'>
+        <div class='col-xs-12'>
+      <div class='page-header'>
+        <h1 class='text-center'>Home Page!</h1>
+        <p>
+        </p>
+      </div>
+    </div>
+    <div class='col-xs-12 col-md-6 col-md-offset-3'>
+      <p>Your Zipcode is <span id="zcode"></span></p>
+      <div class='form-group'>
+        <label for='geocomplete'>Street address (find your reps)</label>
+        <input id='geocomplete' class='form-control' name='location' />
+        <div class='geodata'>
+          <input name="lat" id='lat' type="hidden" value="">
+          <input name="lng" id='lng' type="hidden" value="">
+        </div>
+      </div>
+      <div class='form-group' id='official-group'>
+        <div class="list-group" id="officialsTable">
+        </div>
+      </div>
+      <a href="discussionLookup.html" id="testID" class="btn btn-default" value="[NYL000112]">[NYL000112]</a>
+    </div>
+  </div>  
+</body>
+
+<script>
+  var zipHTML = document.getElementById("zcode");
+  var zipcode = sessionStorage.getItem('zipcode');
+  //console.log(zipcode);
+  zipHTML.innerHTML = zipcode;
+  var discussionLookup = "\"discussionLookup.html\"";
+  
+  var testCode = document.getElementById("testID").innerHTML;
+  console.log(testCode);
+  sessionStorage.setItem('testCode', testCode);
+  
+$(document).ready(function() {
+
+  var API_ROOT = 'http://openstates.org/api/v1/';
+  var API_KEY = '11815f3d1a374d038ab4b7d544b6cc09';
+
+  $('#geocomplete').geocomplete({
+    details: '.geodata'
+  }).bind("geocode:result", function(event, result) {
+    geoLookup($('#lat').val(), $('#lng').val());
+  });
+
+  function geoLookup(lat, lng) {
+    $('#official-group').hide();
+    var endpoint = API_ROOT + 'legislators/geo/?lat=' + lat + '&long=' + lng;
+    $.get(endpoint, {
+      apikey: API_KEY
+    }, function(response) {
+      $('#official-group').show();
+
+      $.each(response, function(i, official) {
+        var listItem = document.createElement('a');
+        listItem.setAttribute('id', 'politician');
+        
+        var link = '<span href="' + official.url + '" target="blank">Official Website »</span>';
+        var politicalID = official.leg_id;
+        var data = official.full_name + ' (' + official.party.charAt(0) + '-' + official.state.toUpperCase() + ')';
+        var name = '<span>' + data + link + '</span>';
+        var img = '<img src="' + official.photo_url + '"/>';
+        
+        listItem.setAttribute('href', "#");
+        listItem.setAttribute('class', "list-group-item");       
+        listItem.innerHTML = img + '<h3 id="' + politicalID + '">' + politicalID + "<br>" + name + '</h3>';
+        listItem.addEventListener('click', function() {
+          var newSearch = this;
+          storeSend(newSearch);
+        });
+
+        $('#officialsTable').append(listItem);
+        
+      });
+    });
+  };
+  
+  // add dynamic link to store shit and send it.
+  function storeSend(politicianInfo) {
+    
+    sessionStorage.setItem('politician', politicianInfo.innerHTML);
+    //console.log("Saved the session stuff");
+    //console.log(sessionStorage.getItem('politician'));
+    location.assign("discussionLookup.html");
+    
+    // discussionLookup.html
+    
+  }
+  
+});
+
+</script>
